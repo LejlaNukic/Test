d17905874b2e1e4f7f61d1920adb94be262bee04
AmarPanjeta <apanjeta1@etf.unsa.ba>
2016-03-13 17:47:34 +0100

    Imamo usere

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
+
   def new
 
   end
 
   def create
       @user=User.new(user_params)
+      if @user.save
+        redirect_to @user
+      else
+        flash[:error]="passwordi se ne poklapaju konju"
+        redirect_to new_user_path
+      end
   end
 
+  def show
+    @user=User.find(params[:id])
+  end
   private
   def user_params
     params.require(:user).permit(:name,:email,:password,:password_confirmation)
